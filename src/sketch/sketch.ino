e2a3b6236d69ca0b78bd5b9693206d01d7e9c6ed
Kalys Osmonov <kalys@osmonov.com>
2014-10-26 13:33:13 +0600

    Bump to 1.0

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -10,22 +10,51 @@ int IN4 = 4;
 // global control params
 char left_wheel_direction, right_wheel_direction;
 int left_wheel_speed, right_wheel_speed;
-char serial_input[10];
+char serial_input[5];
+
+boolean toggle1 = 0;
 
 void setup()
 {
+    
   pinMode (ENA, OUTPUT);
   pinMode (IN1, OUTPUT);
   pinMode (IN2, OUTPUT);
   pinMode (ENB, OUTPUT);
   pinMode (IN3, OUTPUT);
   pinMode (IN4, OUTPUT);
+  // TODO remove
+  pinMode (13, OUTPUT);
+  
+  cli();
+  
+  //set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 4000;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  //OCR1A = (16 * 10^6) / (2 * 1024) - 1;
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  
+  sei();
 
   Serial.begin(9600);
 }
 
+ISR(TIMER1_COMPA_vect){
+  stop();
+}
+
 void move()
 {
+  digitalWrite(13, HIGH);
+  TCNT1  = 0;
   if (left_wheel_direction == 'f') {
     digitalWrite (IN1, HIGH);
     digitalWrite (IN2, LOW);
@@ -49,10 +78,13 @@ void move()
   }
 
   analogWrite(ENA, left_wheel_speed);
-  analogWrite(ENB, left_wheel_speed);
+  analogWrite(ENB, right_wheel_speed);
 }
 
-void stop() {
+void stop() 
+{
+  digitalWrite(13, LOW);
+  left_wheel_direction = right_wheel_direction = 'n';
   analogWrite(ENA, 0);
   analogWrite(ENB, 0);
   digitalWrite (IN1, LOW);
@@ -63,21 +95,22 @@ void stop() {
 
 void loop()
 {
-  int result;
+  int result = 0;
   if( Serial.available() ) {
-    result = Serial.readBytesUntil('\n', serial_input, 5);
+    result = Serial.readBytesUntil('e', serial_input, 5);
   }
 
   if (result != 0) {
-    Serial.write(serial_input);
     left_wheel_direction = serial_input[0];
     right_wheel_direction = serial_input[2];
 
     left_wheel_speed = serial_input[1];
     right_wheel_speed = serial_input[3];
-
-    move();
-    delay(500);
-    stop();
+    if ((left_wheel_direction == 'f' || left_wheel_direction == 'b') && (right_wheel_direction == 'f' || right_wheel_direction == 'b')) {
+      move();
+    }
+    result = 0;
+    serial_input[0] = '\0';
   }
+  delay(20);
 }
